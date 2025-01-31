fc045bd08795f75384e37d5737d1659dcae04f7b
mathias <mathias.lafayedemicheaux@gmail.com>
2025-01-31 15:39:53 +0100

    added multiple floors !

---
+++ layer_switch_normal.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var above_index = 1
+@export var below_index = 0
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.has_method("change_floor"):
+		if body.global_position.y < global_position.y:
+			body.change_floor(above_index)
+		else:
+			body.change_floor(below_index)
