ab5145cee36e163173dfb23394d2cbb117115010
unknown <amaury.chevoir@epfl.ch>
2025-02-01 18:35:46 +0100

    Character working, map sketched

--- violet.gd
+++ violet.gd
@@ -9,50 +9,48 @@ extends CharacterBody2D
 # run : 200
 
 @export var speed_curve: Curve
-@export var idle_anim_speed: float = 1
-@export var walk_coef: float = 1.5
-@export var run_coef: float = 4.5
-@export var move_speed: float = 100
-@export var walk_speed: float = 100
-@export var run_speed: float = 300
-@export var starting_direction: Vector2 = Vector2(0, 1)
+@export_enum("p1", "p2") var id: String = "p2"
+@export var animation_constant:float = 2
 
-@export var walk_duration_threshold: float = 1  # Time required to switch to run
+const idle_coef: float = 0.5
+const max_speed: float = 100
+
+var speed_coef: float = 1
+
+@export var starting_direction: Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
 var input_direction: Vector2
-var running: bool = false
 var walk_timer: float = 0.0
-var move_coef: float = 1.5
-
-var speed
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("idle_N")
 	animation_tree.set("parameters/Idle/BlendSpace2D/blend_position", starting_direction)
 
 func _physics_process(delta: float) -> void:
-	get_direction_vector(input_direction)
-	update_animation_direction(input_direction) # choose between idle and walk
-	speed = compute_speed_coef(delta)
-	velocity = input_direction * move_coef * move_speed
-	
-	pick_new_state(delta) 
+	get_direction_vector() # using inputs
+	update_walk_timer(delta, input_direction) # if walking, counts
+	compute_speed_coef(walk_timer) # accelerate considering timer
+	update_animation(input_direction) # choose direction
+	update_move_speed(speed_coef)
 	
 	move_and_slide()
 	
-func get_direction_vector(input_direction: Vector2):
+func get_direction_vector():
 	input_direction = Vector2(
-		Input.get_action_strength("right_p2") - Input.get_action_strength("left_p2"),
-		Input.get_action_strength("down_p2") - Input.get_action_strength("up_p2")
+		Input.get_action_strength("right_" + id) - Input.get_action_strength("left_" + id),
+		Input.get_action_strength("down_" + id) - Input.get_action_strength("up_" + id)
 	).normalized()
 	
-func update_animation_direction(move_input : Vector2):
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/BlendSpace2D/blend_position", move_input.reflect(Vector2.RIGHT))
-		animation_tree.set("parameters/Idle/BlendSpace2D/blend_position", move_input.reflect(Vector2.RIGHT))
+func update_animation(input_direction : Vector2):
+	if(input_direction != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/BlendSpace2D/blend_position", input_direction.reflect(Vector2.RIGHT))
+		animation_tree.set("parameters/Idle/BlendSpace2D/blend_position", input_direction.reflect(Vector2.RIGHT))
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
 		
 func update_walk_timer(delta: float, input_direction: Vector2) -> void:
 	if input_direction != Vector2.ZERO:
@@ -60,40 +58,16 @@ func update_walk_timer(delta: float, input_direction: Vector2) -> void:
 	else:
 		walk_timer = 0.0
 	
-func compute_speed_coef(delta: float):
-	if(velocity != Vector2.ZERO):
-		if walk_timer >= walk_duration_threshold:
-			running = true
-			$AnimatedSprite2D.set("speed_scale", run_coef)
-			state_machine.travel("Walk")
-			if(move_speed<=run_speed):
-				move_speed += delta
-				
-		else:
-			running = false
-			$AnimatedSprite2D.set("speed_scale", walk_coef)
-			state_machine.travel("Walk")
-			move_speed = walk_speed
+func compute_speed_coef(timer: float):
+	if(timer == 0):
+		speed_coef = idle_coef
 	else:
-		$AnimatedSprite2D.set("speed_scale", idle_coef)
-		state_machine.travel("Idle")
-		running = false
+		speed_coef = speed_curve.sample(remap(timer,0,1,0,1))
 
-func pick_new_state(delta: float):
-	if(velocity != Vector2.ZERO):
-		if walk_timer >= walk_duration_threshold:
-			running = true
-			$AnimatedSprite2D.set("speed_scale", run_coef)
-			state_machine.travel("Walk")
-			if(move_speed<=run_speed):
-				move_speed += delta
-				
-		else:
-			running = false
-			$AnimatedSprite2D.set("speed_scale", walk_coef)
-			state_machine.travel("Walk")
-			move_speed = walk_speed
-	else:
+func update_move_speed(speed_coef: float):
+	if(speed_coef == 0.0):
 		$AnimatedSprite2D.set("speed_scale", idle_coef)
-		state_machine.travel("Idle")
-		running = false
+		velocity = Vector2.ZERO
+	else:
+		$AnimatedSprite2D.set("speed_scale", speed_coef * animation_constant)
+		velocity = speed_coef * max_speed * input_direction
